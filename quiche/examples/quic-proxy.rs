8e49186de5826dd34110353ad351698a7e6e0ac0
Johnson <johnsonli1993@gmail.com>
2023-08-03 16:06:08 +0300

    update quic-proxy

--- quiche/examples/quic-proxy.rs
+++ quiche/examples/quic-proxy.rs
@@ -3,14 +3,12 @@ extern crate log;
 
 use std::collections::HashMap;
 use std::error::Error;
-use std::net::{IpAddr, Ipv4Addr, SocketAddr};
-use std::time::Instant;
+use std::net::{SocketAddr, UdpSocket, IpAddr};
+use dns_parser::RData;
 use env_logger::Builder;
 use log::LevelFilter;
 use quiche::Config;
-use trust_dns_resolver::TokioAsyncResolver;
-use trust_dns_resolver::config::{ResolverConfig, NameServerConfigGroup};
-
+use dns_parser::rdata::a::Record;
 
 const MAX_DATAGRAM_SIZE: usize = 1350;
 
@@ -33,130 +31,137 @@ pub fn init_quic_config() -> Config {
     config
 }
 
-async fn resolve_host(host: &str, resolver: &TokioAsyncResolver) -> Result<String, String> {
-    info!("Resolving {}", host);
-    let response = resolver.lookup_ip(host).await.unwrap();
-    for ip in response.iter() {
-        return Ok(ip.to_string());
-    }
-    return Err(format!("Failed to resolve {}", host));
-}
-
-async fn forward_initial_request(data: Vec<u8>, server: &str, port: u16) -> Result<mio::net::UdpSocket, Box<dyn Error>> {
-    let socket = std::net::UdpSocket::bind(format!("0.0.0.0:{}", port))?;
-    let server_socket = mio::net::UdpSocket::from_socket(socket)?;
-    let server_addr = server.parse()?;
-    let addr = SocketAddr::new(server_addr, 443);
-    server_socket.connect(addr)?;
-    server_socket.send(&data)?;
-    Ok(server_socket)
-}
-
-async fn forward_request(from: SocketAddr, data: Vec<u8>, server_socket: Option<&mio::net::UdpSocket>,
-                         resolver: TokioAsyncResolver) -> Result<Option<mio::net::UdpSocket>, Box<dyn Error>> {
-    if server_socket.is_some() {
-        server_socket.unwrap().send(&data)?;
-        return Ok(None);
-    }
+fn parse_server_name(from: SocketAddr, data: &Vec<u8>) -> Option<String> {
     let mut payload = data.clone();
-    let hdr = quiche::Header::from_slice(&mut payload, quiche::MAX_CONN_ID_LEN)?;
-    let mut config: Config = init_quic_config();
-    let scid = hdr.scid.clone();
-    let mut conn = quiche::accept(&scid, None, from, &mut config)?;
-    let recv_info = quiche::RecvInfo { from };
-    let _read = conn.recv_lite(payload.as_mut(), recv_info)?;
-    let server_name = match conn.server_name() {
-        Some(v) => v,
-        None => {
-            error!("Failed to get server name");
-            return Ok(None);
+    let hdr: quiche::Header<'_> = match quiche::Header::from_slice(&mut payload, quiche::MAX_CONN_ID_LEN) {
+        Ok(v) => v,
+        Err(_) => {
+            error!("Failed to parse header");
+            return None;
         }
     };
-    let dst_ip_str = resolve_host(server_name, &resolver).await?;
-    let sock = forward_initial_request(data, &dst_ip_str, from.port()).await?;
-    Ok(Some(sock))
+    let mut config: Config = init_quic_config();
+    let scid = hdr.scid.clone();
+    let mut conn = match quiche::accept(&scid, None, from, &mut config) {
+        Ok(v) => v,
+        Err(_) => {
+            error!("Failed to accept connection");
+            return None;
+        }
+    };
+    let recv_info = quiche::RecvInfo { from };
+    let _read = match conn.recv_lite(payload.as_mut(), recv_info) {
+        Ok(v) => v,
+        Err(_) => {
+            error!("Failed to recv QUIC packet");
+            return None;
+        }
+    };
+    conn.server_name().map(|name| name.to_string())
 }
 
-async fn forward_response(data: Vec<u8>, client_socket: &mio::net::UdpSocket, target: SocketAddr) -> Result<(), Box<dyn Error>> {
-    client_socket.send_to(&data, &target)?;
-    Ok(())
+type ConnectionMap = HashMap<u16, UdpSocket>;
+type NameResolutionMap = HashMap<String, Vec<(u16, Vec<u8>)>>;
+
+fn dns_query(name: &str, socket: &UdpSocket) {
+    let mut builder = dns_parser::Builder::new_query(0, false);
+    builder.add_question(name, false, dns_parser::QueryType::A, dns_parser::QueryClass::IN);
+    let query = builder.build().unwrap();
+    socket.send(&query).unwrap();
 }
 
-type ConnectionMap = HashMap<u16, mio::net::UdpSocket>;
+fn udp_server() -> Result<(), Box<dyn Error>> {
+    // Init sockets.
+    let client_socket = UdpSocket::bind("0.0.0.0:443")?;
+    client_socket.set_nonblocking(true)?;
+    let dns_socket = UdpSocket::bind("0.0.0.0:0")?;
+    dns_socket.set_nonblocking(true)?;
+    dns_socket.connect(SocketAddr::new("127.0.0.1".parse().unwrap(), 8053))?;
 
-async fn udp_server() -> Result<(), Box<dyn Error>> {
-    // Setup the event loop.
-    let poll = mio::Poll::new().unwrap();
-    let mut events = mio::Events::with_capacity(1024);
-
-    let dns_server_ips: &[IpAddr] = &[IpAddr::V4(Ipv4Addr::new(127,0,0,1))];
-    let name_servers = NameServerConfigGroup::from_ips_clear(dns_server_ips, 8053, true);
-    let resolver_config = ResolverConfig::from_parts(None, vec![], name_servers); 
-    let resolver: TokioAsyncResolver = TokioAsyncResolver::tokio(resolver_config, Default::default())?;
-    Builder::new()
-        .filter(None, LevelFilter::Info)
-        .init();
-    let mut args = std::env::args();
-    let cmd = &args.next().unwrap();
-    if args.len() != 0 {
-        info!("Usage: {}", cmd);
-        info!("\nAn IP proxy serving as the Aeacus resolver.");
-        error!("Invalid arguments");
-        return Ok(());
-    }
     let mut connection_records: ConnectionMap = HashMap::new();
-    let listen_addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(0,0,0,0)), 443);
-    let client_socket = mio::net::UdpSocket::bind(&listen_addr).unwrap();
-    poll.register(
-        &client_socket,
-        mio::Token(0),
-        mio::Ready::readable(),
-        mio::PollOpt::edge(),
-    ).unwrap();
+    let mut resolution_records: NameResolutionMap = HashMap::new();
     let mut recv_buf: [u8; MAX_DATAGRAM_SIZE] = [0; MAX_DATAGRAM_SIZE];
     let mut client_addr = None;
 
     loop {
-        poll.poll(&mut events, None)?;
-        for event in events.iter() {
-            let token = event.token();
-            let port: u16 = token.0 as u16;
-            let socket_to_read = match port {
-                0 => &client_socket,
-                _ => connection_records.get(&port).unwrap()
-            };
-            let (len, addr) = socket_to_read.recv_from(&mut recv_buf)?;
-            info!("Received {} bytes from {}", len, addr);
-            let start_ts = Instant::now();
-            if port == 0 {
+        // Step 1, read from the client
+        match client_socket.recv_from(&mut recv_buf) {
+            Ok((len, addr)) => {
+                let data = recv_buf[..len].to_vec();
                 if client_addr.is_none() {
                     client_addr = Some(addr.ip());
                 }
-                let udp_port = addr.port();
-                let server_socket = connection_records.get(&udp_port);
-                let server_socket = forward_request(addr, recv_buf[..len].to_vec(), server_socket, resolver.clone()).await?;
+                let server_socket: Option<&UdpSocket> = connection_records.get(&addr.port());
                 if server_socket.is_some() {
-                    let server_socket = server_socket.unwrap();
-                    poll.register(&server_socket, mio::Token(udp_port as usize), 
-                                  mio::Ready::readable(), mio::PollOpt::edge()).unwrap();
-                    connection_records.insert(udp_port, server_socket);
+                    // Forward non-initial packets directly to the server
+                    server_socket.unwrap().send(&data).unwrap();
+                } else {
+                    // Perform DNS resolution for initial packets
+                    match parse_server_name(addr, &data) {
+                        Some(name) => {
+                            dns_query(&name, &dns_socket);
+                            if !resolution_records.contains_key(&name) {
+                                resolution_records.insert(name.to_string(), Vec::new());
+                            }
+                            resolution_records.get_mut(&name).unwrap().push((addr.port(), data));
+                        },
+                        _ => { }
+                    };
                 }
-            } else {
-                let target = SocketAddr::new(client_addr.unwrap(), port);
-                forward_response(recv_buf[..len].to_vec(), &client_socket, target).await?;
-            }
-            info!("It takes {:?} to forward packet.", start_ts.elapsed());
+            },
+            _ => { }
+        }
+
+        // Step 2, read from the servers
+        for (port, server_socket) in &connection_records {
+            match server_socket.recv(&mut recv_buf) {
+                Ok(len) => {
+                    let target: SocketAddr = SocketAddr::new(client_addr.unwrap(), *port);
+                    client_socket.send_to(&recv_buf[..len], &target)?;
+                },
+                _ => { }
+            };
+        }
+
+        // Step 3, read from the DNS resolver and forward the initial packets
+        match dns_socket.recv(&mut recv_buf) {
+            Ok(len) => {
+                let data = recv_buf[..len].to_vec();
+                let response = dns_parser::Packet::parse(&data).unwrap();
+                let name = response.questions[0].qname.to_string();
+                for answer in response.answers {
+                    match answer.data {
+                        RData::A(Record(ip)) => {
+                            info!("Create server socket {} for {}", ip, name);
+                            let target: SocketAddr = SocketAddr::new(IpAddr::V4(ip), 443);
+                            match resolution_records.remove(&name) {
+                                Some(list) => {
+                                    for (port, data) in list {
+                                        let server_socket = UdpSocket::bind(format!("0.0.0.0:{}", port))?;
+                                        server_socket.set_nonblocking(true)?;
+                                        server_socket.connect(target)?;
+                                        server_socket.send(&data)?;
+                                        connection_records.insert(port, server_socket);
+                                    }
+                                },
+                                _ => { }
+                            };
+                            break;
+                        }
+                        _ => { }
+                    }
+                }
+            },
+            _ => { }
         }
     }
 }
 
-#[tokio::main]
-async fn main() {
+fn main() -> Result<(), Box<dyn Error>> {
+    Builder::new()
+        .filter(None, LevelFilter::Info)
+        .init();
     info!("Starting QUIC proxy");
-    match udp_server().await {
-        Ok(_) => (),
-        Err(e) => {
-            error!("Failed to start QUIC proxy: {}", e);
-        }
-    }
+    udp_server()?;
+    Ok(())
 }
